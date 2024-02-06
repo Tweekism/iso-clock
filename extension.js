b88ee3dce2ab6183d903f998859f188d4c34a3d4
Jason Fairchild <jason@fairchild.au>
2024-02-06 22:30:41 +1000

    Added Week Day support
    The Gnome setting "Week Day" now prints the current day of the week
    
    Week numbers are now tied to the "Week Numbers" setting instead

--- extension.js
+++ extension.js
@@ -26,16 +26,25 @@ export default class IsoClock extends Extension {
 		}
 
 		const gnomeSettings = Gio.Settings.new("org.gnome.desktop.interface");
+		const gnomeCalendar = Gio.Settings.new("org.gnome.desktop.calendar");
 
 		const formats = [
 			"%H:%M",
-			"%Y-%m-%d %H:%M",
+			"%Y-%m-%d   %H:%M",
 			"%H:%M:%S",
-			"%Y-%m-%d %H:%M:%S",
-			"W%V-%u %H:%M",
-			"%Y-%m-%d  W%V-%u %H:%M",
-			"W%V-%u %H:%M:%S",
-			"%Y-%m-%d  W%V-%u %H:%M:%S",
+			"%Y-%m-%d   %H:%M:%S",
+			"%A   %H:%M",
+			"%A   %Y-%m-%d   %H:%M",
+			"%A   %H:%M:%S",
+			"%A   %Y-%m-%d   %H:%M:%S",
+			"W%V-%u   %H:%M",
+			"%Y-%m-%d   W%V-%u   %H:%M",
+			"W%V-%u   %H:%M:%S",
+			"%Y-%m-%d   W%V-%u %H:%M:%S",
+			"%A   W%V-%u   %H:%M",
+			"%A   %Y-%m-%d   W%V-%u   %H:%M",
+			"%A   W%V-%u %H:%M:%S",
+			"%A   %Y-%m-%d   W%V-%u   %H:%M:%S",
 		];
 
 		const override = () => {
@@ -49,22 +58,29 @@ export default class IsoClock extends Extension {
 			const d = gnomeSettings.get_boolean("clock-show-date");
 			const s = gnomeSettings.get_boolean("clock-show-seconds");
 			const w = gnomeSettings.get_boolean("clock-show-weekday");
+			const n = gnomeCalendar.get_boolean("show-weekdate");
 
-			const format = (d << 0) | (s << 1) | (w << 2);
+			const format = (d << 0) | (s << 1) | (w << 2) | (n << 3);
 
 			this.newClock = now.format(formats[format]);
 			this.defaultClock = this.label.get_text();
 			this.label.set_text(this.newClock);
 		};
 
-		this.handlerid = this.label.connect("notify::text", override);
+		this.labelHandleId = this.label.connect("notify::text", override);
+		// We also need to know when the "Week Numbers" setting changes
+		// when it does, trigger a refresh by setting clock back to its default value
+		this.calendarHandleId = gnomeCalendar.connect("changed::show-weekdate", () => {
+		    this.label.set_text(this.defaultClock); 
+		})
 		override();
 	}
 
 	disable() {
-		if (this.handlerid) {
-			this.label.disconnect(this.handlerid);
-			this.handlerid = null;
+		if (this.labelHandleId) {
+			this.label.disconnect(this.labelHandleId);
+			this.labelHandleId = null;
+			this.calendarHandleId = null;
 
 			this.label.set_text(this.defaultClock);
 			this.label = null;
