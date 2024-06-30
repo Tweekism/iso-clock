4855b6cc46b9d25e9fccd038b47ecaf61a15ce5b
Jason Fairchild <jason@fairchild.au>
2024-06-30 19:50:24 +1000

    Tabs to spaces

--- extension.js
+++ extension.js
@@ -5,92 +5,92 @@ import GLib from "gi://GLib";
 import Gio from "gi://Gio";
 
 export default class IsoClock extends Extension {
-	enable() {
-		const dateMenu = Main.panel.statusArea.dateMenu;
+    enable() {
+        const dateMenu = Main.panel.statusArea.dateMenu;
 
-		const clockDisplayBox = dateMenu
-			.get_children()
-			.find((x) => x.style_class === "clock-display-box");
+        const clockDisplayBox = dateMenu
+            .get_children()
+            .find((x) => x.style_class === "clock-display-box");
 
-		this.label = clockDisplayBox?.get_children().find(
-			(x) =>
-				x.style_class === "clock" &&
-				// Make sure it's (hopefully) the clock
-				// by checking for "∶" (\u2236) (not ascii ":")
-				x.text?.includes("\u2236")
-		);
+        this.label = clockDisplayBox?.get_children().find(
+            (x) =>
+                x.style_class === "clock" &&
+                // Make sure it's (hopefully) the clock
+                // by checking for "∶" (\u2236) (not ascii ":")
+                x.text?.includes("\u2236")
+        );
 
-		if (!this.label) {
-			console.error("No clock label? Aborting.");
-			return;
-		}
+        if (!this.label) {
+            console.error("No clock label? Aborting.");
+            return;
+        }
 
-		const gnomeSettings = Gio.Settings.new("org.gnome.desktop.interface");
-		const gnomeCalendar = Gio.Settings.new("org.gnome.desktop.calendar");
+        const gnomeSettings = Gio.Settings.new("org.gnome.desktop.interface");
+        const gnomeCalendar = Gio.Settings.new("org.gnome.desktop.calendar");
 
-		const override = () => {
-			// Don't do anything if the clock label hasn't actually changed
-			if (this.newClock == this.label.get_text()) {
-				return;
-			}
+        const override = () => {
+            // Don't do anything if the clock label hasn't actually changed
+            if (this.newClock == this.label.get_text()) {
+                return;
+            }
 
-			const now = GLib.DateTime.new_now_local();
+            const now = GLib.DateTime.new_now_local();
 
-			let day, date, week, time;
+            let day, date, week, time;
 
-			if (gnomeSettings.get_boolean("clock-show-weekday")) {
-				day = "%A"
-			}
+            if (gnomeSettings.get_boolean("clock-show-weekday")) {
+                day = "%A"
+            }
 
-			if (gnomeSettings.get_boolean("clock-show-date")) {
-				date = "%Y-%m-%d";
-			}
+            if (gnomeSettings.get_boolean("clock-show-date")) {
+                date = "%Y-%m-%d";
+            }
 
-			if (gnomeCalendar.get_boolean("show-weekdate")) {
-				week = "W%V-%u"
-			}
+            if (gnomeCalendar.get_boolean("show-weekdate")) {
+                week = "W%V-%u"
+            }
 
-			if (gnomeSettings.get_string("clock-format") === '24h') {
-				time = "%H:%M";
-			} else {
-				time = "%I:%M %p";
-			}
+            if (gnomeSettings.get_string("clock-format") === '24h') {
+                time = "%H:%M";
+            } else {
+                time = "%I:%M %p";
+            }
 
-			if (gnomeSettings.get_boolean("clock-show-seconds")) {
-				time = time.replace("%M","%M:%S");
-			}
+            if (gnomeSettings.get_boolean("clock-show-seconds")) {
+                time = time.replace("%M","%M:%S");
+            }
 
-			const format = [day, date, week, time].filter(v => v).join("   ");
+            const format = [day, date, week, time].filter(v => v).join("   ");
 
-			this.newClock = now.format(format);
-			this.defaultClock = this.label.get_text();
-			this.label.set_text(this.newClock);
-		};
+            this.newClock = now.format(format);
+            this.defaultClock = this.label.get_text();
+            this.label.set_text(this.newClock);
+        };
 
-		this.labelHandleId = this.label.connect("notify::text", override);
-		// We also need to know when the "Week Numbers" setting changes
-		// when it does, trigger a refresh by setting clock back to its default value
-		this.calendarHandleId = gnomeCalendar.connect("changed::show-weekdate", () => {
-		    this.label.set_text(this.defaultClock); 
-		})
-		override();
-	}
+        this.labelHandleId = this.label.connect("notify::text", override);
+        // We also need to know when the "Week Numbers" setting changes
+        // when it does, trigger a refresh by setting clock back to its default value
+        this.calendarHandleId = gnomeCalendar.connect("changed::show-weekdate", () => {
+            this.label.set_text(this.defaultClock);
+        })
+        override();
+    }
 
-	disable() {
-		if (this.calendarHandleID) {
-			this.label.disconnect(this.calendarHandleID);
-			this.calendarHandleID = null;
+    disable() {
+        if (this.calendarHandleID) {
+            this.label.disconnect(this.calendarHandleID);
+            this.calendarHandleID = null;
                 }
 
-		if (this.labelHandleId) {
-			this.label.disconnect(this.labelHandleId);
-			this.labelHandleId = null;
+        if (this.labelHandleId) {
+            this.label.disconnect(this.labelHandleId);
+            this.labelHandleId = null;
 
-			this.label.set_text(this.defaultClock);
-			this.label = null;
+            this.label.set_text(this.defaultClock);
+            this.label = null;
 
-			this.newClock = null;
-			this.defaultClock = null;
-		}
-	}
+            this.newClock = null;
+            this.defaultClock = null;
+        }
+    }
 }
