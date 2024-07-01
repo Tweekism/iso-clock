281e5a6643f7efbb74a7435d97c0a76fcac782ed
Jason Fairchild <jason@fairchild.au>
2024-07-01 12:08:43 +1000

    V5
    Lots of code clean up and fixed a bug disconnecting the calendar
    settings signal

--- extension.js
+++ extension.js
@@ -1,3 +1,6 @@
+// This extenstion is originally based on S410's ISO8601-ish Clock
+// https://gitlab.com/S410/iso8601ish
+
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
@@ -26,7 +29,7 @@ export default class IsoClock extends Extension {
         }
 
         const gnomeSettings = Gio.Settings.new("org.gnome.desktop.interface");
-        const gnomeCalendar = Gio.Settings.new("org.gnome.desktop.calendar");
+        this.gnomeCalendar = Gio.Settings.new("org.gnome.desktop.calendar");
 
         const override = () => {
             // Don't do anything if the clock label hasn't actually changed
@@ -34,8 +37,7 @@ export default class IsoClock extends Extension {
                 return;
             }
 
-            const now = GLib.DateTime.new_now_local();
-
+            // Setup the custom clock format based on the clock settings in Gnome Settings
             let day, date, week, time;
 
             if (gnomeSettings.get_boolean("clock-show-weekday")) {
@@ -46,7 +48,7 @@ export default class IsoClock extends Extension {
                 date = "%Y-%m-%d";
             }
 
-            if (gnomeCalendar.get_boolean("show-weekdate")) {
+            if (this.gnomeCalendar.get_boolean("show-weekdate")) {
                 week = "W%V-%u"
             }
 
@@ -62,25 +64,34 @@ export default class IsoClock extends Extension {
 
             const format = [day, date, week, time].filter(v => v).join("   ");
 
-            this.newClock = now.format(format);
+            // Keep a copy of the default clock text so that we can revert it when the
+            // extension is disabled
             this.defaultClock = this.label.get_text();
+
+            // Set the clock label to our new custom format
+            const now = GLib.DateTime.new_now_local();
+            this.newClock = now.format(format);
             this.label.set_text(this.newClock);
         };
 
+        // Whenever the clock label updates override with our custom clock format
         this.labelHandleId = this.label.connect("notify::text", override);
-        // We also need to know when the "Week Numbers" setting changes
-        // when it does, trigger a refresh by setting clock back to its default value
-        this.calendarHandleId = gnomeCalendar.connect("changed::show-weekdate", () => {
+
+        // We also need to know when the "Week Numbers" setting changes, as week numbers
+        // don't appear in the default clock. Trigger a refresh by setting clock back to 
+        // its default value. This prevents an edge case where disabling the extension 
+        // after a week number setting change causes unexpected behaviour
+        this.calendarHandleId = this.gnomeCalendar.connect("changed::show-weekdate", () => {
             this.label.set_text(this.defaultClock);
         })
         override();
     }
 
     disable() {
-        if (this.calendarHandleID) {
-            this.label.disconnect(this.calendarHandleID);
-            this.calendarHandleID = null;
-                }
+        if (this.calendarHandleId) {
+            this.gnomeCalendar.disconnect(this.calendarHandleId);
+            this.calendarHandleId = null;
+        }
 
         if (this.labelHandleId) {
             this.label.disconnect(this.labelHandleId);
